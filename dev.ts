a8bcdaae55de4f8b9a23e19ec98e1cf9b0565d3a
Nikitap <nikita.philippe@etu.univ-nantes.fr>
2025-02-10 21:17:24 +0100

    🚧 Test if daisy ui works on deploy

--- dev.ts
+++ dev.ts
@@ -10,4 +10,14 @@ if (!Deno.args.includes("build")) {
     await cron.default();
 }
 
+import autoprefixer from "autoprefixer";
+import postcss from "postcss";
+import tailwindcss from "@tailwindcss/postcss";
+
+const css = Deno.readTextFileSync("./static/app.css");
+const out = await postcss([tailwindcss, autoprefixer]).process(css, {
+  from: "./static/app.css",
+});
+Deno.writeTextFileSync("./static/styles.css", out.css);
+
 await dev(import.meta.url, "./main.ts", config);
