e9ba589500a4bfe8b7cdfa6a9e486168e722f41f
Nikitap <nikita.philippe@etu.univ-nantes.fr>
2025-02-21 18:42:55 +0100

    🐛 Fix cron checking on ms instead of days

--- utils/reminder.ts
+++ utils/reminder.ts
@@ -4,17 +4,12 @@ import { getEntry } from "@utils/entries.ts";
 
 export const checkForDailyAnswer = async () => {
   const lastEntry = await getEntry();
-  
-  console.log("Last entry:", lastEntry);
-  console.log("Infos:", {
-    hasLastEntry: !!lastEntry,
-    lastEntryAt: lastEntry?.at,
-    now: DateTime.now(),
-    isEntryBeforeNow: lastEntry ? DateTime.fromISO(lastEntry.at) < DateTime.now().minus({ days: 1 }) : "notfound",
-    isSending: !lastEntry || DateTime.fromISO(lastEntry.at) < DateTime.now().minus({ days: 1 }),
-  });
 
-  if (!lastEntry || DateTime.fromISO(lastEntry.at) < DateTime.now().minus({ days: 1 })) {
+  // Compare if last entry is before current one, by DAY
+  const lastEntryDate = DateTime.fromISO(lastEntry?.at ?? '1970-01-01').startOf('day')
+  const currentEntryDate = DateTime.now().minus({ days: 1 }).startOf('day')
+
+  if (lastEntryDate < currentEntryDate) {
     const res = await sendDiscordPushNotification({ content: "Answer CCV! 📝" });
     return res.ok;
   }
