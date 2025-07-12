e70a39d7c495f58928bee4894df454cdbc307d14
Nikitap <nikita.philippe@etu.univ-nantes.fr>
2025-07-12 15:22:06 +0200

    🍱 Add simple CCV logo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,10 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>ccv</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/logo/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/logo/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/logo/favicon-16x16.png" />
+        <link rel="manifest" href="/logo/site.webmanifest" />
         <link rel="stylesheet" href="/styles.css" />
         <script src="/loader.js" defer></script>
       </head>
