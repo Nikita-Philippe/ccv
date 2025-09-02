7adfc23d56c42aec4eb8003a41642ae4f4f19eaf
Nikitap <nikita.philippe@etu.univ-nantes.fr>
2025-09-02 20:16:01 +0200

    👥 Add troispetitsbruns logo contributor

--- components/Homepage/Footer.tsx
+++ components/Homepage/Footer.tsx
@@ -17,18 +17,33 @@ export default function Footer() {
           All rights reserved
         </a>
       </div>
-      <a
-        class="absolute right-6 2xl:right-[20vw] top-1/2 -translate-y-1/2"
-        href="https://github.com/Nikita-Philippe"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          src="https://avatars.githubusercontent.com/u/54618484?v=4"
-          alt="Nikita Philippe"
-          className="rounded-full w-8 h-8"
-        />
-      </a>
+      <div class="absolute right-6 2xl:right-[20vw] top-1/2 -translate-y-1/2 flex flex-col gap-0.5 items-end">
+        <a
+          class="w-fit"
+          href="https://github.com/Nikita-Philippe"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://avatars.githubusercontent.com/u/54618484?v=4"
+            alt="Nikita Philippe"
+            className="rounded-full w-8 h-8"
+          />
+        </a>
+        <div class="text-xs opacity-70 flex gap-0.5">
+          <p class="hidden sm:block">
+            Logo by
+          </p>
+          <a
+            class="link"
+            href="https://www.instagram.com/troispetitsbruns"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            troispetitsbruns
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
